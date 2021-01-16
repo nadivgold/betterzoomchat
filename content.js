41004fc1bbb14adfb317c6a7e2413b0dcda2835d
Nadiv Gold <nadivge@gmail.com>
2021-01-16 15:54:30 -0700

    initial commit with mostly working code

---
+++ content.js
@@ -0,0 +1,157 @@
+var t = {
+  ':Kappa:': 'https://www.twitch.tv/creatorcamp/assets/uploads/kappa.png',
+  ':HeyGuys:': 'https://www.twitch.tv/creatorcamp/assets/uploads/unnamed.png',
+  ':LUL:': 'https://www.twitch.tv/creatorcamp/assets/uploads/lul.png',
+  ':CoolStoryBob:': 'https://www.twitch.tv/creatorcamp/assets/uploads/bob.png',
+  ':4Head:': 'https://www.twitch.tv/creatorcamp/assets/uploads/4head.png',
+  ':FrankerZ:': 'https://www.twitch.tv/creatorcamp/assets/uploads/frankerz.png',
+  ':Jebaited:': 'https://www.twitch.tv/creatorcamp/assets/uploads/jebaited.png',
+  ':PJSalt:': 'https://www.twitch.tv/creatorcamp/assets/uploads/salt.png',
+  ':NotLikeThis:': 'https://www.twitch.tv/creatorcamp/assets/uploads/notlikethis.png',
+  ':MrDestructoid:': 'https://static-cdn.jtvnw.net/emoticons/v1/28/1.0',
+  ':WutFace:': 'https://www.twitch.tv/creatorcamp/assets/uploads/wutface.png',
+  ':VoHiYo:': 'https://www.twitch.tv/creatorcamp/assets/uploads/vohiyo.png',
+  ':SeemsGood:': 'https://www.twitch.tv/creatorcamp/assets/uploads/seemsgood.png',
+  ':ResidentSleeper:': 'https://www.twitch.tv/creatorcamp/assets/uploads/resident-sleeper.png',
+  ':Kreygasm:': 'https://static-cdn.jtvnw.net/emoticons/v1/41/1.0',
+  ':TriHard:': 'https://static-cdn.jtvnw.net/emoticons/v1/120232/1.0',
+  ':RedCoat:': 'https://static-cdn.jtvnw.net/emoticons/v1/22/1.0',
+  ':chipsaKEK:': 'https://static-cdn.jtvnw.net/emoticons/v1/305010854/1.0',
+  ':nakoGalante:': 'https://static-cdn.jtvnw.net/emoticons/v1/302287630/1.0',
+  ':BabyRage:': 'https://static-cdn.jtvnw.net/emoticons/v1/22639/1.0',
+  ':BibleThump:': 'https://static-cdn.jtvnw.net/emoticons/v1/86/1.0',
+  ':SMOrc:': 'https://static-cdn.jtvnw.net/emoticons/v1/52/1.0',
+  ':SwiftRage:': 'https://static-cdn.jtvnw.net/emoticons/v1/34/1.0',
+  ':DansGame:': 'https://static-cdn.jtvnw.net/emoticons/v1/33/1.0',
+}
+
+var b = {
+  'monkaS': 'https://cdn.betterttv.net/emote/56e9f494fff3cc5c35e5287e/1x',
+  'OMEGALUL': 'https://cdn.betterttv.net/emote/583089f4737a8e61abb0186b/1x',
+  'Clap': 'https://cdn.betterttv.net/emote/55b6f480e66682f576dd94f5/1x',
+  'POGGERS': 'https://cdn.betterttv.net/emote/58ae8407ff7b7276f8e594f2/1x',
+  'PepeHands': 'https://cdn.betterttv.net/emote/59f27b3f4ebd8047f54dee29/1x',
+  'catJAM': 'https://cdn.betterttv.net/emote/5f1b0186cf6d2144653d2970/1x',
+  'EZ': 'https://cdn.betterttv.net/emote/5590b223b344e2c42a9e28e3/1x',
+  'pepeJAM':'https://cdn.betterttv.net/emote/5b77ac3af7bddc567b1d5fb2/1x',
+  'Pepega': 'https://cdn.betterttv.net/emote/5aca62163e290877a25481ad/1x',
+  'PepePls': 'https://cdn.betterttv.net/emote/55898e122612142e6aaa935b/1x',
+  'pepeD': 'https://cdn.betterttv.net/emote/5b1740221c5a6065a7bad4b5/1x',
+  'KEKW': 'https://cdn.betterttv.net/emote/5e9c6c187e090362f8b0b9e8/1x',
+  'PogU': 'https://cdn.betterttv.net/emote/5e4e7a1f08b4447d56a92967/1x',
+  'gachiBASS': 'https://cdn.betterttv.net/emote/57719a9a6bdecd592c3ad59b/1x',
+  '5Head': 'https://cdn.betterttv.net/emote/5d6096974932b21d9c332904/1x',
+  'AYAYA': 'https://cdn.betterttv.net/emote/58493695987aab42df852e0f/1x',
+  'gachiHYPER': 'https://cdn.betterttv.net/emote/59143b496996b360ff9b807c/1x',
+  'PepeLaugh': 'https://cdn.betterttv.net/emote/5c548025009a2e73916b3a37/1x',
+  'weirdChamp': 'https://cdn.betterttv.net/emote/5d20a55de1cfde376e532972/1x',
+  'blobDance': 'https://cdn.betterttv.net/emote/5ada077451d4120ea3918426/1x',
+  'monkaW': 'https://cdn.betterttv.net/emote/59ca6551b27c823d5b1fd872/1x',
+  'pepeLaugh': 'https://cdn.betterttv.net/emote/59b73909b27c823d5b1f6052/1x',
+  'ricardoFlick': 'https://cdn.betterttv.net/emote/5bc2143ea5351f40921080ee/1x',
+  'PepegaAim': 'https://cdn.betterttv.net/emote/5d0d7140ca4f4b50240ff6b4/1x',
+  'Sadge': 'https://cdn.betterttv.net/emote/5e0fa9d40550d42106b8a489/1x',
+  'peepoClap': 'https://cdn.betterttv.net/emote/5d38aaa592fc550c2d5996b8/1x',
+  'PepoDance': 'https://cdn.betterttv.net/emote/5a6edb51f730010d194bdd46/1x',
+  'HYPERS': 'https://cdn.betterttv.net/emote/5980af4e3a1ac5330e89dc76/1x',
+  'RainbowPls': 'https://cdn.betterttv.net/emote/5b35cae2f3a33e2b6f0058ef/1x',
+  'sumSmash': 'https://cdn.betterttv.net/emote/5af84b9e766af63db43bf6b9/1x',
+  'TriDance': 'https://cdn.betterttv.net/emote/5d1e70f498539c4801cc3811/1x',
+  'LULW': 'https://cdn.betterttv.net/emote/5dc79d1b27360247dd6516ec/1x',
+  'Dance': 'https://cdn.betterttv.net/emote/5aa1d0e311237146531078b0/1x',
+  '(ditto)': 'https://cdn.betterttv.net/emote/554da1a289d53f2d12781907/1x',
+  'peepoLeave': 'https://cdn.betterttv.net/emote/5d324913ff6ed36801311fd2/1x',
+  'monkaHmm': 'https://cdn.betterttv.net/emote/5aa16eb65d4a424654d7e3e5/1x',
+  'peepoHappy': 'https://cdn.betterttv.net/emote/5a16ee718c22a247ead62d4a/1x',
+  'peepoArrive': 'https://cdn.betterttv.net/emote/5d922afbc0652668c9e52ead/1x',
+  'gachiGASM': 'https://cdn.betterttv.net/emote/55999813f0db38ef6c7c663e/1x',
+  'pepeDS': 'https://cdn.betterttv.net/emote/5b444de56b9160327d12534a/1x',
+  'pressF': 'https://cdn.betterttv.net/emote/5c857788f779543bcdf37124/1x',
+  'monkaTOS': 'https://cdn.betterttv.net/emote/5a7fd054b694db72eac253f4/1x',
+}
+var f = {
+  'Pog': 'https://cdn.frankerfacez.com/emoticon/210748/1',
+  'OMEGALUL': 'https://cdn.frankerfacez.com/emoticon/128054/1',
+  'PepeHands': 'https://cdn.frankerfacez.com/emoticon/231552/1',
+  'LULW': 'https://cdn.frankerfacez.com/emoticon/139407/1',
+  'Pepega': 'https://cdn.frankerfacez.com/emoticon/243789/1',
+  'widepeepoHappy': 'https://cdn.frankerfacez.com/emoticon/270930/1',
+  'monkaW': 'https://cdn.frankerfacez.com/emoticon/214681/1',
+  '5Head': 'https://cdn.frankerfacez.com/emoticon/239504/1',
+  'monkaS': 'https://cdn.frankerfacez.com/emoticon/130762/1',
+  'POGGERS': 'https://cdn.frankerfacez.com/emoticon/214129/1',
+  'KEKW': 'https://cdn.frankerfacez.com/emoticon/381875/1',
+  'AYAYA': 'https://cdn.frankerfacez.com/emoticon/162146/1',
+  'PepeLaugh': 'https://cdn.frankerfacez.com/emoticon/64785/1',
+  'FeelsBadMan': 'https://cdn.frankerfacez.com/emoticon/33355/1',
+  'widepeepoSad': 'https://cdn.frankerfacez.com/emoticon/303899/1',
+  'FeelsOkayMan': 'https://cdn.frankerfacez.com/emoticon/145947/1',
+  'Kapp': 'https://cdn.frankerfacez.com/emoticon/218860/1',
+  'HYPERS': 'https://cdn.frankerfacez.com/emoticon/236895/1',
+  'HandsUp': 'https://cdn.frankerfacez.com/emoticon/229760/1',
+  'gachiGASM': 'https://cdn.frankerfacez.com/emoticon/103171/1',
+  '3Head': 'https://cdn.frankerfacez.com/emoticon/274406/1',
+  'FeelsWeirdMan': 'https://cdn.frankerfacez.com/emoticon/131597/1',
+  'EZY': 'https://cdn.frankerfacez.com/emoticon/185890/1',
+  'YEP': 'https://cdn.frankerfacez.com/emoticon/418189/1',
+  'Sadge': 'https://cdn.frankerfacez.com/emoticon/425196/1',
+  'PepoThink': 'https://cdn.frankerfacez.com/emoticon/174942/1',
+  'KKomrade': 'https://cdn.frankerfacez.com/emoticon/145916/1',
+  'monkaHmm': 'https://cdn.frankerfacez.com/emoticon/240746/1',
+  'HYPERBRUH': 'https://cdn.frankerfacez.com/emoticon/204717/1',
+  'forsenCD': 'https://cdn.frankerfacez.com/emoticon/249060/1',
+  'monkaOMEGA': 'https://cdn.frankerfacez.com/emoticon/167431/1',
+  'monkaTOS': 'https://cdn.frankerfacez.com/emoticon/237857/1',
+  'monkaGun': 'https://cdn.frankerfacez.com/emoticon/187256/1',
+  'FeelsGoodMan': 'https://cdn.frankerfacez.com/emoticon/109777/1',
+  'monkaEyes': 'https://cdn.frankerfacez.com/emoticon/268204/1',
+  'peepoHappy': 'https://cdn.frankerfacez.com/emoticon/228449/1'
+}
+
+chrome.runtime.onMessage.addListener(
+  function(request, sender, sendResponse) {
+    // debugger;
+    if(request.method === "launchMeeting"){
+      window.location.replace("https://"+window.location.hostname+(window.location.pathname.replace('j', 'wc/join'))+window.location.search);
+    }
+    else if(request.method === "none")
+      console.log('none selected');
+    else {
+      let twitch = bttv = ffz = false;
+
+      if(request.method.includes('twitch'))
+        twitch = true;
+      if(request.method.includes('bttv'))
+        bttv = true;
+      if(request.method.includes('ffz'))
+        ffz = true;
+      
+      var interval;
+      clearInterval(interval);
+
+      interval = setInterval(() => {
+        const query = document.querySelectorAll('pre');
+        for(let p in query){
+          let m = query[p].innerHTML;
+          if(m){
+            let words = m.split(/[^a-zA-Z0-9]+/gi);
+            for(let word = 0; word < words.length; word++){
+              let w = words[word].replace(/[^0-9a-z]/gi, ''); //cleans the word
+              console.log(w) //testing
+              if(twitch && (w in t)){
+                m = m.replace(w, `<img src=${t[w]}>`);
+              }
+              else if(bttv && (w in b)){
+                m = m.replace(w, `<img src=${b[w]}>`);
+              }
+              else if(ffz && (w in b)){
+                m = m.replace(w, `<img src=${f[w]}>`);
+              }
+            }
+          }
+          query[p].innerHTML = m;
+        }
+      }, 100);
+    }
+  }
+);
---
+++ popup.js
@@ -0,0 +1,88 @@
+var twitchEmotes = true;
+chrome.storage.sync.get(['twitch'], function(result) {
+  twitchEmotes = result.twitch;
+  document.getElementById('twitch').checked = twitchEmotes;
+});
+var bttvEmotes = true;
+chrome.storage.sync.get(['bttv'], function(result) {
+  bttvEmotes = result.bttv;
+  document.getElementById('bttv').checked = bttvEmotes;
+});
+var ffzEmotes = true;
+chrome.storage.sync.get(['ffz'], function(result) {
+  ffzEmotes = result.ffz;
+  document.getElementById('ffz').checked = ffzEmotes;
+});
+var settingsOpen = false;
+var infoOpen = false;
+
+function send(payload) {
+  chrome.tabs.query({active: true, currentWindow: true}, (tabs) => {
+    chrome.tabs.sendMessage(tabs[0].id, {method: `${payload}`});
+  });
+}
+
+document.addEventListener('DOMContentLoaded', function() {
+  // Settings Switches
+  var twichToggle = document.getElementById('twitch');
+  twichToggle.addEventListener('click', () => {
+    twitchEmotes = !twitchEmotes;
+    chrome.storage.sync.set({twitch: twitchEmotes}, function() {
+      console.log('Twitch Value is set to ' + twitchEmotes);
+    });
+  });
+  var bttvToggle = document.getElementById('bttv');
+  bttvToggle.addEventListener('click', () => {
+    bttvEmotes = !bttvEmotes;
+    chrome.storage.sync.set({bttv: bttvEmotes}, function() {
+      console.log('bttv Value is set to ' + bttvEmotes);
+    });
+  });
+  var ffzToggle = document.getElementById('ffz');
+  ffzToggle.addEventListener('click', () => {
+    ffzEmotes = !ffzEmotes;
+    chrome.storage.sync.set({ffz: ffzEmotes}, function() {
+      console.log('ffz Value is set to ' + ffzEmotes);
+    });
+  });
+  // Settings Dropdown
+  var settingsDropdown = document.getElementById('set');
+  settingsDropdown.addEventListener('click', () => {
+    settingsOpen = !settingsOpen;
+    if(settingsOpen){
+      settingsDropdown.innerHTML = "Settings △"
+      document.getElementById('settings').style.display = 'block';
+    } else {
+      settingsDropdown.innerHTML = "Settings ▽"
+      document.getElementById('settings').style.display = 'none';
+    }
+  }, false);
+  // Info Dropdown
+  var infoDropdown = document.getElementById('help');
+  infoDropdown.addEventListener('click', () => {
+    infoOpen = !infoOpen;
+    if(infoOpen){
+      infoDropdown.innerHTML = "Info △"
+      document.getElementById('info').style.display = 'block';
+    } else {
+      infoDropdown.innerHTML = "Info ▽"
+      document.getElementById('info').style.display = 'none';
+    }
+  }, false);
+  // Start Button
+  var checkButton = document.getElementById('check');
+  checkButton.addEventListener('click', () => {
+    if(twitchEmotes || bttvEmotes || ffzEmotes)
+      send(`${ffzEmotes ? 'ffz' : ''} ${bttvEmotes ? 'bttv' : ''} ${twitchEmotes ? 'twitch' : ''}`)
+    else
+      send('none')
+  }, false);
+
+  // Launch Meeting Button
+  var launchMeeting = document.getElementById('launchMeeting');
+  launchMeeting.addEventListener('click', () => {
+    chrome.tabs.query({active: true, currentWindow: true}, (tabs) => {
+      chrome.tabs.sendMessage(tabs[0].id, {method: `launchMeeting`});
+    });
+    }, false);
+}, false);
